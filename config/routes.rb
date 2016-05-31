37ace347de017ff000a619962184cf76157f4580
Aafke Smal <aafke.smal@gmail.com>
2016-05-31 15:54:57 +0200

    taskmanager

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,18 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :tasks
+  # # display a list of all tasks
+  # get '/tasks', to: "tasks#index"
+  # # return an form to create a new tasks
+  # post '/tasks', to: "tasks#create", as: :task
+  # # Show one task
+  # get '/tasks/:id', to: "tasks#show"
+  # # edit a task
+  # patch "/tasks/:id", to: "tasks#update"
+  # # delete a task
+  # delete "/tasks/:id", to: "tasks#destroy"
+  # # return a form to edit a task
+  # get '/tasks/new', to: "tasks#new", as: :new_task
+  # # create a new task
+  # get '/tasks/:id/edit', to: "tasks#edit", as: :edit_task
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Task.create(name: "Lizette bellen", description: "Over het verhuizen", completed: false)
+Task.create(name: "hosting checken", description: "2 juni", completed: false)
+Task.create(name: "FB-event aanmaken", description: "11 juni?", completed: true)
+Task.create(name: "Marktplaats", description: "berichies", completed: false)
+Task.create(name: "Lunch", description: "tosti", completed: true)
